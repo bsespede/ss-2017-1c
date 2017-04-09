b7c9c4aae7e8a5151d60d3fe31ee9a3434e4e8d6
Braulio Sespede <bsespede@itba.edu.ar>
2017-04-09 04:31:17 -0300

    Reasonable numbers

--- tp2/src/Simulator/Simulation.java
+++ tp2/src/Simulator/Simulation.java
@@ -194,7 +194,7 @@ public class Simulation {
 	
 	private double[][][] calculateVelocities() {
     	// Para calcular velocidad media uso coarse-graining
-    	int grainSize = 125;
+    	int grainSize = 32;
     	int grainX = width / grainSize;
     	int grainY = height / grainSize;
     	
