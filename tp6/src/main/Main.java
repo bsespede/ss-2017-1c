1bced94e105bc656cfba79462a462f468ef4cdb0
bsespede <bsespede@itba.edu.ar>
2017-06-03 17:41:23 -0300

    TP6 - Ej4 matlab+plot+movie

--- tp6/src/main/Main.java
+++ tp6/src/main/Main.java
@@ -1,4 +1,9 @@
 package main;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import io.ResultWriter;
 import simulation.Result;
 import simulation.Simulation;
@@ -15,25 +20,25 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("[INFO] Starting simulation");
 		// Ejercicio A, B, C
-		for (int runId = 0; runId < 10; runId++) {
-			final Simulation simulation = new Simulation(runId, INTEGRATOR, INTERVAL, INTERVAL_OUTPUT, N, 0.8d);
-			final Result result = simulation.simulate();
-			ResultWriter.writeResult("../" + runId + "-", result);
-		}
-		// Ejercicio D
-//		int runId = 0;
-//		final Map<Double, List<Double>> evacTimes = new HashMap<>();
-//		for (double drivingSpeed = 0.8; drivingSpeed <= 6; drivingSpeed += (6.0 - 0.8) / 10.0) {
-//			int times = 10;
-//			final List<Double> evacuationTimeForSpeed = new ArrayList<>(times);
-//			for (int run = 0; run < times; run++) {
-//				final Simulation simulation = new Simulation(runId++, INTEGRATOR, INTERVAL, INTERVAL_OUTPUT, N, drivingSpeed);
-//				final Result result = simulation.simulate();
-//				evacuationTimeForSpeed.add(result.getEvacuationTime());
-//			}
-//			evacTimes.put(drivingSpeed, evacuationTimeForSpeed);
+//		for (int runId = 0; runId < 10; runId++) {
+//			final Simulation simulation = new Simulation(runId, INTEGRATOR, INTERVAL, INTERVAL_OUTPUT, N, 0.8d);
+//			final Result result = simulation.simulate();
+//			ResultWriter.writeResult("../" + runId + "-", result);
 //		}
-//		ResultWriter.writeEvacTimes("../", evacTimes);
+		// Ejercicio D
+		int runId = 0;
+		final Map<Double, List<Double>> evacTimes = new HashMap<>();
+		for (double drivingSpeed = 0.8; drivingSpeed <= 6; drivingSpeed += (6.0 - 0.8) / 10.0) {
+			int times = 10;
+			final List<Double> evacuationTimeForSpeed = new ArrayList<>(times);
+			for (int run = 0; run < times; run++) {
+				final Simulation simulation = new Simulation(runId++, INTEGRATOR, INTERVAL, INTERVAL_OUTPUT, N, drivingSpeed);
+				final Result result = simulation.simulate();
+				evacuationTimeForSpeed.add(result.getEvacuationTime());
+			}
+			evacTimes.put(drivingSpeed, evacuationTimeForSpeed);
+		}
+		ResultWriter.writeEvacTimes("../", evacTimes);
 		System.out.println("[INFO] Simulations ended");
 	}
 
---
+++ tp6/src/matlab/ej4.m
@@ -0,0 +1,22 @@
+function plotEvactuationSpeeds() 
+
+  [speed time] = textread("../../results/4/total-evacuations.dat", "%f %f");
+  
+  %resultSpeed average minTime maxTime
+  results = zeros(10, 4);
+  for current = 10:10:size(time)
+    results(current/10, 1) = speed(current, 1);
+    results(current/10, 2) = mean(time(current-9:current, 1));
+    results(current/10, 3) = min(time(current-9:current, 1));
+    results(current/10, 4) = max(time(current-9:current, 1));
+  endfor  
+  
+  errorbar(results(:, 1), results(:, 2), results(:, 4) - results(:, 3), ".");
+  title("Tiempo de evacuacion total en funcion de velocidad deseada");
+  xlabel("Velocidad deseada (m/s)");
+  ylabel("Tiempo de evacuacion total (s)");
+  print("../../plots/ej4.png", "-dpng");
+  
+endfunction
+
+plotEvactuationSpeeds();
